00d0851811fc5106b491a7c5405f32358512d68f
muhammad hafizh <mhafizh7121@gmail.com>
2024-11-28 22:52:10 +0700

    update last for today

--- public/script.js
+++ public/script.js
@@ -12,8 +12,8 @@ let lastUpdate = {
 function getData() {
   const currentTime = Date.now(); // Waktu saat ini dalam milidetik
   $.ajax({
-    // url: "/api/sensors",
-    url: "https://hydroponic-dashboard-zcsw.vercel.app/api/sensors",
+    url: "/api/sensors",
+    // url: "https://hydroponic-dashboard-zcsw.vercel.app/api/sensors",
 
     type: "GET",
     success: function (data) {
